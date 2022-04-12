37421a68c78150d0054f8f3e312fe530c8879333
Pravin-Dhumal <iampravin.dhumal@gmail.com>
2022-04-12 15:06:57 +0530

    project api

--- controller/productController.js
+++ controller/productController.js
@@ -60,6 +60,31 @@ const createProduct = async function(req, res){
     }
 }
 
+/// get Product by id ------------------------------------
+
+const getProduct = async function (req, res) {
+
+    try {
+      const productId = req.params.productId
+      if (!(isValid(productId))) { return res.status(400).send({ status: false, message: "productId is required" }) }
+      if (!isValidObjectId(productId)) { return res.status(400).send({ status: false, message: "Valid productId is required" }) }
+  
+  
+      const product = await productModel.findOne({ _id: productId, isDeleted: false })
+      if (!product) { return res.status(400).send({ status: false, message: "No data found" }) }
+      
+      return res.status(200).send({ status: true, message: "Product Data", data: product })
+    }
+    catch (err) {
+      console.log(err)
+      res.status(500).send({ message: err.message })
+    }
+  }
+  
+  module.exports.getProduct = getProduct
+  
+
+
 /****************************delete product******************************/
 
 
