4409954085e2de3630f698ccc8804ed15c69f85f
satilasha <swatighosh2111@gmail.com>
2022-04-13 10:44:36 +0530

    authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,13 +7,17 @@ const authentication = (req, res, next) => {
         if (!token) {
             return res.status(400).send({ status: false, msg: "please enter token " })
         }
-
-        let decoded = jwt.verify(token, "Group26")
-
-        if (!decoded) {
-            return res.status(403).send({ status: false, mag: "token is not valid" })
+        let decodeToken = jwt.decode(token)
+        if (!decodeToken) {
+            return res.status(401).send({ status: false, msg: "Not a valid Token " })
         }
+        if((Date.now() > (decodeToken.exp * 1000))){
+            return res.status(403).send({status : false, message : `session expired, please login again`})
+            }
+        jwt.verify(token,  "this-is-aSecretTokenForLogin");
+        req.loggedInUser = decodeToken.userId
         next()
+       
     }
     catch (err) {
         return res.status(500).send({ status: false, msg: err.message })
